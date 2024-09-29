91715121b0c34e11675a201ad54b6724c38a2186
Layze <layzegot@gmail.com>
2024-09-29 17:38:26 +0700

    adding footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import Logo from '../../assets/logo.svg'; 
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          {/* Logo Section */}
+          <div className="col-lg-3 col-md-6 mb-4">
+            <img src={Logo} alt="FPT TTKoi Logo" className="footer-logo" />
+          </div>
+
+          {/* Get Help Section */}
+          <div className="col-lg-2 col-md-6 mb-4">
+            <h5>Get Help</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Order status</a></li>
+              <li><a href="#">FAQs</a></li>
+              <li><a href="#">Terms & Condition</a></li>
+            </ul>
+          </div>
+
+          {/* Features Section */}
+          <div className="col-lg-2 col-md-6 mb-4">
+            <h5>Features</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Pond</a></li>
+              <li><a href="#">Shop</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Blogs</a></li>
+            </ul>
+          </div>
+
+          {/* About Us Section */}
+          <div className="col-lg-2 col-md-6 mb-4">
+            <h5>About Us</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">About FPT TTKoi</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <br></br>
+              <li style={{ color: 'gray', fontSize: '36px', fontWeight: 'bold'}}>FPT TTKoi</li>
+            </ul>
+          </div>
+
+          {/* Contact Section */}
+          <div className="col-lg-2 col-md-6 mb-4">
+            <h5>Contact</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Email</a></li>
+              <li><a href="#">Hotline</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <hr/>
+
+        {/* Bottom Section */}
+        <div className="row">
+          <div className="col-12 text-center">
+            <p className="copyright">© 2024 SWP391 - FPT University - Fall 2024. All rights reserved</p>
+            <p className="address">Address: E2-a-7, D1 Street, Long Thanh My District, Thu Duc City, Ho Chi Minh City</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
