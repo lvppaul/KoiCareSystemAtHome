450fee98d95450aa718e03ce30f21afb4e046930
enteecaay <kietnguyen060204@gmail.com>
2024-10-02 09:34:53 +0700

    fix conflict app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,15 +2,14 @@ import "./App.css";
 import Nav from "./components/Navbar/Navigationbar";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
-import Pond from './page/Pond/Pond';
-import AdminHome from './page/AdminHome/AdminHome';
 
 function App() {
   return (
     <>
-      <div className='navbar'><Nav /></div>
-      <div className='main-content'>
-
+      <div className="navbar">
+        <Nav />
+      </div>
+      <div className="main-content">
         <Outlet />
       </div>
       <div className="footer">
