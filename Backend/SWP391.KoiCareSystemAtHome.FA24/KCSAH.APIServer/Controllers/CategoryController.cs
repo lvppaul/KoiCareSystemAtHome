b65f2bc0cb301b10067d113a40d23ed5ac0683ed
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2024-10-08 21:45:18 +0700

    add new project

---
+++ Backend/SWP391.KoiCareSystemAtHome.FA24/KCSAH.APIServer/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SWP391.KCSAH.Repository.Models;
+using SWP391.KCSAH.Repository;
+
+namespace KCSAH.APIServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public CategoryController(UnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetAllSync()
+        {
+            return await _unitOfWork.CategoryRepository.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetByIdAsync(string id)
+        {
+            var result = await _unitOfWork.CategoryRepository.GetByIdAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(Category category)
+        {
+            try
+            {
+                await _unitOfWork.CategoryRepository.CreateAsync(category);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(500, "Error saving category");
+            }
+
+            return CreatedAtAction("GetCategoryById", new { id = category.CategoryId }, category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(string id, Category category)
+        {
+            if (!id.Equals(category.CategoryId))
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _unitOfWork.CategoryRepository.UpdateAsync(category);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var category = await _unitOfWork.CategoryRepository.GetByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.CategoryRepository.RemoveAsync(category);
+
+            return NoContent();
+        }
+        private bool CategoryExists(string id)
+        {
+            return _unitOfWork.CategoryRepository.GetByIdAsync(id) == null;
+        }
+    }
+}
+
---
+++ Backend/SWP391.KoiCareSystemAtHome.FA24/SWP391.KCSAH.Repository/KCSAH.Repository/KoiRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWP391.KCSAH.Repository.Base;
+using SWP391.KCSAH.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.KCSAH.Repository.KCSAH.Repository
+{
+    public class KoiRepository : GenericRepository<Koi>
+    {
+        public KoiRepository(KoiCareSystemAtHomeContext context) => _context = context;
+
+        public async Task<List<Koi>> GetAllAsync()
+        {
+            return await _context.Kois.Include(p => p.User).Include(p => p.Pond).ToListAsync();
+        }
+
+        public async Task<Koi> GetByIdAsync(string id)
+        {
+            var result = await _context.Kois.Include(p => p.User).Include(p => p.Pond).FirstAsync(p => p.KoiId.Equals(id));
+
+            return result;
+        }
+
+    }
+}
