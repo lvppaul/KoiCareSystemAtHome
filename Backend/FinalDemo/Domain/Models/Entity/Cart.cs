1aac87e02286af8110dd03107a14f1db33592df9
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2024-10-17 22:31:42 +0700

    Add Model Cart CartItem

---
+++ Backend/FinalDemo/Domain/Models/Entity/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entity
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public virtual ICollection<List<CartItem>> CartItems { get; set; }
+    }
+}
