0c4ade9f3152d983238923a94056d351a2153485
Lê Vĩnh Phát <levinhphat790@gmail.com>
2024-10-17 21:18:43 +0700

    update account function

--- Backend/FinalDemo/Domain/Base/IAccountRepository.cs
+++ Backend/FinalDemo/Domain/Base/IAccountRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Domain.Authentication;
 using Domain.Models;
-using Domain.Models.Entity;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
@@ -11,25 +10,22 @@ using System.Threading.Tasks;
 namespace Domain.Base
 {
     public interface IAccountRepository
-    {   
-
-
-        // Account tools
-        public Task<IdentityResult> SignUpAsync(SignUpModel model);
-        public Task<IdentityResult> CreateShopAccount(SignUpModel model);
-        public Task<IdentityResult> CreateAdminAccount(SignUpModel model);
-        public Task<IdentityResult> CreateVipAccount(SignUpModel model);
+    {
         public Task<string> SignInAsync(SignInModel model);
+        public Task<string> SignUpAsync(SignUpModel model); 
+        public Task<string> CreateShopAccount(SignUpModel model); 
+        public Task<string> CreateVipAccount(SignUpModel model);
+        public Task<string> CreateAdminAccount(SignUpModel model);
+        public Task<string> RequestPasswordReset(string email);
+        public Task<string> ResetPassword(string email, string token, NewPasswordModel model);
         public Task<string> ChangePasswordAsync(string userId,ChangePasswordModel model);
+        public Task<string> ConfirmEmailAsync(string email, string code);
         public Task<string> UpdateAccountDetailAsync(string userId, AccountDetailModel model);
         public Task<string> ChangeRoleToVipAsync(string userId);
         public Task<string> LockoutEnabled(string userId);
         public Task<string> LockoutDisabled(string userId);
-        public Task<bool> CheckLockoutEnabledAsync(ApplicationUser user);
-
-        // Account Data
-        //public Task<IdentityResult>
-
+        //public Task<bool> CheckLockoutEnabledAsync(ApplicationUser user);
+       
 
     }
 }
