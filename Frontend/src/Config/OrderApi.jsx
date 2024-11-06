dc193de883da71ae8460ad08bb2effba73afd068
Lê Vĩnh Phát <levinhphat790@gmail.com>
2024-11-06 16:57:47 +0700

    new order

--- Frontend/src/Config/OrderApi.jsx
+++ Frontend/src/Config/OrderApi.jsx
@@ -1,34 +1,36 @@
-import api from './AxiosConfig';
+import api from "./AxiosConfig";
 
 // Function to create an order
 const createOrder = async (orderData) => {
-    try {
-        const response = await api.post('Order', orderData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Error creating order:', error);
-        return error.response;
-    }
+  try {
+    const response = await api.post("Order", orderData, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error creating order:", error);
+    return error.response;
+  }
 };
 
 const getOrderById = async (orderId) => {
-    try {
-        const response = await api.get(`Order/${orderId}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching order:', error);
-        return null;
-    }
-}
+  try {
+    const response = await api.get(`Order/${orderId}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching order:", error);
+    return null;
+  }
+};
 
 export { createOrder, getOrderById };
+
+// Get List Vip Package Order.
