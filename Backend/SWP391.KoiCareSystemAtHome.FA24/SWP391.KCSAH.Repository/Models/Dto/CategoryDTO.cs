789a4fe30100d29d03c8d3c8eff61e3c8d11b2bd
Lê Vĩnh Phát <levinhphat790@gmail.com>
2024-11-20 22:31:26 +0700

    delete old project

--- Backend/SWP391.KoiCareSystemAtHome.FA24/SWP391.KCSAH.Repository/Models/Dto/CategoryDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KCSAH.APIServer.Dto
-{
-    public class CategoryDTO
-    {
-        public string CategoryId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
